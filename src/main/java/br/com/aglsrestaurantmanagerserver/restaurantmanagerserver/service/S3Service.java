35847b2528d8a82032f463ec8d82333a6edbdb3a
André G. Leão da Silva <54383652+andreleao1@users.noreply.github.com>
2023-04-07 07:43:33 -0300

    improve error resolution to upload food photo

--- src/main/java/br/com/aglsrestaurantmanagerserver/restaurantmanagerserver/service/S3Service.java
+++ src/main/java/br/com/aglsrestaurantmanagerserver/restaurantmanagerserver/service/S3Service.java
@@ -1,7 +1,9 @@
 package br.com.aglsrestaurantmanagerserver.restaurantmanagerserver.service;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +16,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 @Service
+@Slf4j
 public class S3Service {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(S3Service.class);
 
     @Value("${cloud.aws.s3.foodbucketname}")
     private String bucketName;
@@ -27,8 +28,14 @@ public class S3Service {
     public String uploadFile(MultipartFile multipartFile) {
         File file = convertToFile(multipartFile);
         String fileName = System.currentTimeMillis() + "_" + multipartFile.getOriginalFilename();
-        s3Client.putObject(new PutObjectRequest(bucketName, fileName,file));
-        file.delete();
+        try{
+            s3Client.putObject(new PutObjectRequest(bucketName, fileName,file));
+        }catch (AmazonS3Exception e) {
+            log.error("Error to upload file");
+            throw new AmazonS3Exception(e.getMessage());
+        } finally {
+            file.delete();
+        }
         return fileName;
     }
 
@@ -37,7 +44,7 @@ public class S3Service {
         try(FileOutputStream fileOutputStream = new FileOutputStream(convertedFile)) {
             fileOutputStream.write(file.getBytes());
         } catch (IOException e) {
-            LOGGER.error("Error converting multipartfile to file", e);
+            log.error("Error converting multipartfile to file", e);
         }
         return convertedFile;
     }
