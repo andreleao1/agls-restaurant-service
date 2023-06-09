2d9736c3fbb8b02fa04642cbac0102aa81365ff3
andreleao1 <andre.gustavo.ls@hotmail.com>
2023-06-09 11:19:55 -0300

    unit tests in category flow is done

--- src/test/java/br/com/aglsrestaurantmanagerserver/restaurantmanagerserver/service/CategoryServiceTest.java
+++ src/test/java/br/com/aglsrestaurantmanagerserver/restaurantmanagerserver/service/CategoryServiceTest.java
@@ -3,6 +3,7 @@ package br.com.aglsrestaurantmanagerserver.restaurantmanagerserver.service;
 import br.com.aglsrestaurantmanagerserver.restaurantmanagerserver.entity.Category;
 import br.com.aglsrestaurantmanagerserver.restaurantmanagerserver.repository.CategoryRepository;
 import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -51,4 +54,72 @@ public class CategoryServiceTest {
         assertThatThrownBy(() -> this.categoryService.save(category))
                 .isInstanceOf(EntityExistsException.class);
     }
+
+    @Test
+    @DisplayName("Update a category")
+    public void mustUpdateACategoryWhenGivenAValidCategory() {
+        Long categoryId = 1l;
+        Category category = Category.builder().id(categoryId).name("TEST").build();
+        Category newCategory = Category.builder().id(categoryId).name("TEST_2").build();
+
+        when(this.categoryRepository.existsByName(category.getName())).thenReturn(false);
+        when(this.categoryRepository.findById(categoryId)).thenReturn(Optional.of(category));
+        when(this.categoryRepository.save(Mockito.any(Category.class))).thenReturn(Category.builder().id(categoryId).name(newCategory.getName()).build());
+
+        Category categoryUpdated = this.categoryService.save(category);
+
+        assertThat(categoryUpdated.getId()).isEqualTo(categoryId);
+        assertThat(categoryUpdated.getName()).isEqualTo(newCategory.getName());
+        verify(this.categoryRepository, times(1)).save(Mockito.any(Category.class));
+    }
+
+    @Test
+    @DisplayName("Find one category by id")
+    public void mustReturnACategoryWhenOneValidIdIsProvided() {
+        Category category = Category.builder().id(1l).name("TEST").build();
+
+        when(this.categoryRepository.findById(category.getId())).thenReturn(Optional.of(category));
+
+        Category categoryFound = this.categoryService.findById(category.getId());
+
+        verify(this.categoryRepository, times(1)).findById(category.getId());
+        assertThat(categoryFound).isEqualTo(category);
+    }
+
+    @Test
+    @DisplayName("Throws an error to try find a category nonexistent")
+    public void mustThrowAnEntityNotFoundExceptionWhenTryFindACategoryWithNonexistentId() {
+        Category category = Category.builder().id(1l).name("TEST").build();
+        when(this.categoryRepository.findById(category.getId())).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> this.categoryService.findById(category.getId()))
+                .isInstanceOf(EntityNotFoundException.class);
+    }
+
+    @Test
+    @DisplayName("Find one category by name")
+    public void mustReturnACategoryWhenOneValidNameIsProvided() {
+        String categoryName = "TEST";
+        Category category = Category.builder().id(1l).name(categoryName).build();
+
+        when(this.categoryRepository.findByName(categoryName)).thenReturn(Optional.of(category));
+
+        Category categoryFound = this.categoryService.findByName(categoryName);
+
+        verify(this.categoryRepository, times(1)).findByName(categoryName);
+        assertThat(categoryFound).isEqualTo(category);
+    }
+
+    @Test
+    @DisplayName("delete by id")
+    public void mustDeleteACategoryWhenOneValidIdIsProvided() {
+        Long categoryId = 1l;
+
+        when(this.categoryRepository.findById(categoryId)).thenReturn(Optional.of(Category.builder().build()));
+
+        this.categoryService.deleteById(categoryId);
+
+        verify(this.categoryRepository, times(1)).deleteById(categoryId);
+    }
+
 }
